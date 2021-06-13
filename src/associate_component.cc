95c80c82290f325bb8149bcd9e8bdc6d4d104faa
Chiram <liangzinan21@hotmail.com>
2021-06-13 18:26:03 +0800

    backup

--- src/associate_component.cc
+++ src/associate_component.cc
@@ -1,1 +1,16 @@
 #include "associate_component.h"
+using namespace rime;
+an<Translation> AssociateTranslator::Query(const string& input,const Segment& segment) {
+  return New<AssociateTranslation>();
+}
+void AssociateTranslator::Record() {} 
+AssociateTranslator::~AssociateTranslator() {}
+
+bool AssociateTranslation::Next() {
+  return false;
+}
+bool AssociateSegmentor::Proceed(Segmentation* segmentation){
+  return false;
+}
+
+an<Candidate> AssociateTranslation::Peek() { return New<SimpleCandidate>(); }
--- src/phraser_module.cc
+++ src/phraser_module.cc
@@ -4,6 +4,7 @@
 //
 
 #include "phraser.h"
+#include "associate_component.h"
 #include <rime/common.h>
 #include <rime/registry.h>
 #include <rime_api.h>
@@ -14,8 +15,11 @@ static void rime_grammar_initialize() {
   LOG(INFO) << "registering components from module 'grammar'.";
   Registry& r = Registry::instance();
   r.Register("grammar", new Component<Phraser>);
+  r.Register("associate_processor", new Component<AssociateProcessor>);
+  r.Register("associate_translator", new Component<AssociateTranslator>);
+  r.Register("associate_segmentor", new Component<AssociateSegmentor>);
 }
 
 static void rime_grammar_finalize() {}
 
-//RIME_REGISTER_MODULE(grammar)
+RIME_REGISTER_MODULE(grammar)

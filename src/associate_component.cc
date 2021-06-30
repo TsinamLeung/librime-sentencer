042ffe6176000546a19ada723f0c570a6c9afd52
Zenam <liangzinan21@hotmail.com>
2021-07-01 01:03:12 +0800

    when multi-character prefix searching, some unexcpted word would came up. But luckliy Associate Perform normal when you trying to push single character into it.

--- src/associate_component.cc
+++ src/associate_component.cc
@@ -1,21 +1,105 @@
 #include "associate_component.h"
 using namespace rime;
-an<Translation> AssociateTranslator::Query(const string& input,const Segment& segment) {
-  return New<AssociateTranslation>();
+an<Translation> AssociateTranslator::Query(const string& input, const Segment& segment) {
+	if (segment.HasTag("associate") && this->env->Memory) {
+		// action could be done in Translation
+		return New<AssociateTranslation>(*this->env, this->result_limit);
+	}
+	return New<AssociateTranslation>();
 }
-void AssociateTranslator::Record() {} 
+void AssociateTranslator::Record(Context* ctx) {
+	if (!ctx) return;
+	this->env->Prefix = ctx->GetCommitText();
+}
+void AssociateTranslator::Record(const string& input) { this->env->Prefix = input; }
+
 AssociateTranslator::~AssociateTranslator() {}
 
 bool AssociateTranslation::Next() {
-  return false;
+	while (!candidates.empty()) {
+		auto iter = candidates.front().second;
+		if (!iter.Next())
+		{
+			candidates.pop();
+			continue;
+		}
+		return true;
+	}
+	set_exhausted(true);
+	return false;
 }
-bool AssociateSegmentor::Proceed(Segmentation* segmentation){
-  return false;
+// let let space input tag as "associate"
+bool AssociateSegmentor::Proceed(Segmentation* segmentation) {
+	if (this->env->IsTrigger) {
+		this->env->IsTrigger = false;
+		auto seg = Segment(0, 1); // input: " "
+		seg.tags.insert("associate"); // insert tag
+		segmentation->Forward();
+		segmentation->AddSegment(seg);
+		return false;
+	}
+	return true;
 }
 
-an<Candidate> AssociateTranslation::Peek() { return New<SimpleCandidate>(); }
+an<Candidate> AssociateTranslation::Peek() {
+	if (candidates.empty())
+		return nullptr;
+	auto candidate = candidates.front();
+	auto& prefix = candidate.first;
+	an<DictEntry>& result = candidate.second.Peek();
+	if (!result)
+	{
+		candidates.pop();
+		return nullptr;
+	}
+	return New<SimpleCandidate>("associate", 0, 1, result->text.substr(prefix.size()), "");
+}
 
-bool rime::AssociateMemory::Memorize(const CommitEntry& entry) { 
-  this->callback_(entry);
-  return true; 
+void rime::AssociateTranslation::FetchWords(const string& prefix, Dictionary* dict, int result_limit)
+{
+	if (result_limit <= 0 || !prefix.size())
+	{
+		return;
+	}
+	if (!dict) {
+		LOG(ERROR) << "Cannot find dictionary";
+		return;
+	}
+
+	DictEntryIterator iter;
+	dict->LookupWords(&iter, prefix, true, result_limit);
+	// find nothing
+	if (!iter.entry_count()) {
+		// exhaust the prefix
+		return FetchWords(prefix.substr(1), dict, result_limit);;
+	}
+
+	iter.Next(); // the first result are itself usually.
+	if (iter.exhausted()) {
+		return;
+	}
+
+	result_limit -= iter.entry_count() - 1;
+	candidates.push(make_pair(prefix, iter));
+
+	//fuck theutf8
+	return FetchWords(prefix.substr(1),dict,result_limit);
+}
+
+
+
+
+bool rime::AssociateMemory::Memorize(const CommitEntry& entry) {
+	this->callback_(entry);
+	return true;
+}
+
+ProcessResult rime::AssociateProcessor::ProcessKeyEvent(const KeyEvent& key_event)
+{
+	if (this->hotkey == key_event) {
+		this->env->IsTrigger = true;
+		this->engine_->context()->set_input(" ");
+		return kAccepted;
+	}
+	return kNoop;
 }
--- src/associate_component.h
+++ src/associate_component.h
@@ -13,40 +13,25 @@
 #include <rime/schema.h>
 #include <rime/gear/memory.h>
 #include <functional>
+#include <rime/key_event.h>
+#include <rime/context.h>
+#include <rime/dict/dictionary.h>
+#include <queue>
+#include "utf8.h"
 
 namespace rime {
 	class Engine;
 	class Schema;
 	class Config;
 	class Memory;
+	class Context;
+	class Dictionary;
 
-	// use to store Global information for the associate Component
-	class AssociateGlobal {
-	public:
-		AssociateGlobal() {};
-
-	};
-
-
-	// AssociateComponent Can inject the global varible to each Component
-	template<typename T>
-	class AssociateComponent : public T::Component {
-	private:
-		an<AssociateGlobal> env_;
-	public:
-		AssociateComponent(an< AssociateGlobal> env) : env_(env) {};
-		T* Create(const Ticket &t) {
-			Ticket a(t.engine,t.name_space,t.name_space);
-			return new T(t,env_);
-		}
-	};
-
-
+	// used to load dictionary	
 	class AssociateMemory : public Memory {
 	public:
-		AssociateMemory(const Ticket& ticket) : Memory(ticket) {
-			
-		}
+		AssociateMemory(const Ticket& ticket) : Memory(ticket) {}
+
 		virtual bool Memorize(const CommitEntry&);
 
 		void SetMemorizeCallback(const std::function<void(const CommitEntry&)>& func) {
@@ -58,30 +43,60 @@ namespace rime {
 		std::function<void(const CommitEntry&)> callback_;
 	};
 
+	// use to store Global information for the associate Component
+	class AssociateGlobal {
+	public:
+		AssociateGlobal() {};
+		bool IsTrigger = false;
+		string Prefix = "";
+		KeyEvent hotkey;
+		const string Namespace = "associate";
+		an<AssociateMemory> Memory = nullptr;
+	};
+
+
+	// AssociateComponent Can inject the global varible to each Component
+	template<typename T>
+	class AssociateComponent : public T::Component {
+	private:
+		an<AssociateGlobal> env_;
+	public:
+		AssociateComponent(an< AssociateGlobal> env) : env_(env) {};
+		T* Create(const Ticket& t) {
+			Ticket a(t.engine, t.name_space, t.name_space);
+			return new T(t, env_);
+		}
+	};
+
 	class AssociateTranslator :
 		public Translator {
 	public:
-		explicit AssociateTranslator( const Ticket& ticket,an<AssociateGlobal> env) : Translator(ticket) {
+		explicit AssociateTranslator(const Ticket& ticket, an<AssociateGlobal> env) : Translator(ticket), env(env) {
+			LOG(INFO) << "Associate Component Namespace " << env->Namespace;
 
-			if (ticket.name_space == "translator") {
-				LOG(INFO) << "Setting namespace to associate";
-				this->name_space_ = "associate";
-			}
 			if (this->engine_) {
 				auto schema = this->engine_->schema();
 				if (schema) {
-					auto config = schema->config();
+					Config* config = schema->config();
 					if (config) {
-						config->GetString(this->name_space_ + "/dictionary", &this->dictionary_name);
-						config->GetInt(this->name_space_ + "/result_limit", &this->result_limit);
+						config->GetString(this->env->Namespace + "/schema", &this->dictionary_name);
+						config->GetInt(this->env->Namespace + "/result_limit", &this->result_limit);
 						LOG(INFO) << "Loading associate Dictionary:[" << this->dictionary_name << "] with result limit:" << this->result_limit;
+
 						Ticket customTicket = Ticket(this->engine_, "translator", "associate_translator");
 						Schema schema(this->dictionary_name);
 						customTicket.schema = &schema;
 
-						this->memory = New<AssociateMemory>(customTicket);
-						this->env = env;
-						this->memory->SetMemorizeCallback(std::bind(&AssociateTranslator::callbackTest,this,std::placeholders::_1));
+						this->env->Memory = New<AssociateMemory>(customTicket);
+
+						// record the commited text
+						Context* ctx = this->engine_->context();
+						if (ctx) {
+							auto f1 = std::bind(
+								static_cast<void(AssociateTranslator::*)(Context*)>(&AssociateTranslator::Record),
+								this, std::placeholders::_1);
+							ctx->commit_notifier().connect(f1);
+						}
 					}
 					else {
 						LOG(ERROR) << "Config doesn't exist";
@@ -98,48 +113,69 @@ namespace rime {
 		virtual ~AssociateTranslator();
 		virtual an<Translation> Query(const string& input,
 			const Segment& segment);
-		virtual void Record();
-
+		virtual void Record(Context*);
+		virtual void Record(const string&);
 	protected:
-		void callbackTest(const CommitEntry& entry) {
-			printf("sAY FUCK THE WORLD\n");
-		}
 		string dictionary_name;
 		int result_limit = 200;
-		an<AssociateMemory> memory = nullptr;
 		an<AssociateGlobal> env = nullptr;
 	};
 
 	class AssociateTranslation : public Translation {
 	public:
-		AssociateTranslation() {};
-
+		AssociateTranslation() { set_exhausted(true); };
+		AssociateTranslation(const AssociateGlobal& global, int result_limit) {
+			FetchWords(global.Prefix, global.Memory->dict() ,result_limit);
+		}
 		// A translation may contain multiple results, looks
 		// something like a generator of candidates.
 		virtual bool Next();
 
 		virtual an<Candidate> Peek();
+
+		void FetchWords(const string& search_prefix,Dictionary* dict, int result_limit);
+	private:
+		std::queue<pair<string,DictEntryIterator>> candidates;
 	};
 
+	//this segmentor use to tag the segment with specified tag that can be recevied by translator
 	class AssociateSegmentor : public Segmentor {
 	public:
-		explicit AssociateSegmentor(const Ticket& ticket,an<AssociateGlobal> env)
-			: Segmentor(ticket) {
+		explicit AssociateSegmentor(const Ticket& ticket, an<AssociateGlobal> env)
+			: Segmentor(ticket), env(env) {
 			this->env = env;
 		}
 
 		virtual bool Proceed(Segmentation* segmentation);
 	protected:
 		an<AssociateGlobal> env;
+
 	};
 
 	class AssociateProcessor : public Processor {
 	public:
-		explicit AssociateProcessor( const Ticket& ticket,an< AssociateGlobal> env) : Processor(ticket) {
-			this->env = env;
+		explicit AssociateProcessor(const Ticket& ticket, an< AssociateGlobal> env) : Processor(ticket), env(env) {
+			if (this->engine_) {
+				auto schema = this->engine_->schema();
+				if (schema) {
+					auto config = schema->config();
+					if (config) {
+						string hotkeyString = "";
+						config->GetString(this->env->Namespace + "/hotkey", &hotkeyString);
+						KeySequence k("{" + hotkeyString + "}");
+						this->hotkey = k.front();
+						if (this->env) {
+							this->env->hotkey = this->hotkey;
+						}
+					}
+				}
+			}
 		}
+
+		virtual ProcessResult ProcessKeyEvent(const KeyEvent& key_event);
 	protected:
 		an< AssociateGlobal>env;
+		KeyEvent hotkey;
 	};
 
 }
